7de7dbe1a68f129bffdf2b4d7ae78e87f13b5385
wdn97 <wiktor.nagorski@gmail.com>
2018-03-13 12:44:35 +0000

    Update binaryFileReader.java

--- binaryFileReader.java
+++ binaryFileReader.java
@@ -1,118 +1,55 @@
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Base64;
 import java.util.Random;
 
-import javax.crypto.Cipher;
-import javax.crypto.SecretKey;
-import javax.xml.bind.DatatypeConverter;
-
-
 public class binaryFileReader {
-	
-	public static void main(String[] args){
-		
-		int possibleCombinations = (int) Math.pow(2,16);
-		byte[][] allKeys = new byte[possibleCombinations][]; 
+	final static int BUFFER_SIZE = 32;
+
+	public static void main(String[] args) {
+		String filePath = "/home/wiktor/eclipse-workspace/Coursework2/src/puzzles.bin";
+		byte[][] data = readBinaryFile(filePath);
+		byte[] message = randomMessagePicker(filePath);
+
+		int possibleCombinations = (int) Math.pow(2, 16);
+		byte[][] allKeys = new byte[possibleCombinations][];
 
 		for (int i = 0; i < possibleCombinations; i++) {
-			
-			allKeys[i] = Arrays.copyOf(CryptoLib.smallIntToByteArray(i),8);
-			  
+
+			allKeys[i] = Arrays.copyOf(CryptoLib.smallIntToByteArray(i), 8);
+			System.out.println(CryptoLib.getHexStringRepresentation(allKeys[i]));
+
 		}
-	
-		
-		byte[] message = randomMessagePicker("/home/wiktor/eclipse-workspace/Coursework2/src/puzzles.bin");
-		
-		
-		
-	
-		
-		
 
 	}
-	
-	
-	
-	
-	
-	
-	
-	
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	public static byte[] randomMessagePicker(String filePath)	
-	{
-	
-		
-		Random random = new Random();
-		int Low = 0;
-		int High = 4096;
-		int result = random.nextInt(High-Low) + Low;
-		
-		byte[] message = readBinaryFile(filePath)[result];
-
-		
-		
-		return message;
-		
-	}
-	
-	
-	public static byte[][] readBinaryFile (String filePath) {
-		
-		File file = new File(filePath); // e.g. "/home/wiktor/eclipse-workspace/Coursework2/src/puzzles.bin"		
-		final int BUFFER_SIZE = 32; 	
+	public static byte[][] readBinaryFile(String filePath) {
+		File file = new File(filePath);
 		long fileSize = file.length();
+		byte[] buffer = new byte[BUFFER_SIZE];
 		byte[][] data = null;
-		
 		FileInputStream fileInputStream;
-		
+
 		try {
 			fileInputStream = new FileInputStream(file);
-			 byte[] allBytes = new byte[(int) fileSize];
-			 fileInputStream.read(allBytes);
+			data = new byte[(int) (fileSize / BUFFER_SIZE)][BUFFER_SIZE];
 
-			 int rest = allBytes.length % BUFFER_SIZE;  
-			 
-			 int chunks = allBytes.length / BUFFER_SIZE + (rest > 0 ? 1 : 0); 
-			 
-			  data = new byte[chunks][]; 
-			    
-			    for(int i = 0; i < (rest > 0 ? chunks - 1 : chunks); i++){
-			        data[i] = Arrays.copyOfRange(allBytes, i * BUFFER_SIZE, i * BUFFER_SIZE + BUFFER_SIZE);
-			    }
-			    if(rest > 0){ 			        
-			        data[chunks - 1] = Arrays.copyOfRange(allBytes, (chunks - 1) * BUFFER_SIZE, (chunks - 1) * BUFFER_SIZE + rest);
-			    }
-			    
-			 // todo use the buffer way
-			 
+			for (int i = 0; i < (fileSize / BUFFER_SIZE); i++) {
+				fileInputStream.read(buffer);
+				data[i] = Arrays.copyOf(buffer, BUFFER_SIZE);
+			}
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.print("Caught exception : " + e.getMessage());
 		}
-		
-
 		return data;
-				
 	}
-		
+
+	public static byte[] randomMessagePicker(String filePath) {
+		long fileSize = new File(filePath).length();
+		Random random = new Random();
+		int randomNumber = random.nextInt((int) (fileSize / BUFFER_SIZE));
+		byte[] message = readBinaryFile(filePath)[randomNumber];
+		return message;
+	}
 }
